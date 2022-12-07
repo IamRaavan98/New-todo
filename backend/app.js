fe7c527aae52fa2025b29cd6e213f3266843f99b
Rohan Agrawal <Rohanagrawal1798@gmail.com>
2022-12-07 17:55:15 +0530

    tempfile

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express")
+const app = express();
+const DBconnection = require("./config/DBs")
+const routes  = require("./routes/routes")
+const cookieParser = require('cookie-parser')
+const cors = require("cors")
+// connecting to database
+
+DBconnection();
+
+//middleware
+app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+// app.get('/addtodo', (req, res) => {
+//     res.send('Hello !')
+
+//   })
+app.use("/",routes)
+
+
+module.exports = app;
+
+
